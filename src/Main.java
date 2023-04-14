087927ce2cdcd54bba74b5c122d166ea00e5d229
yasminalmeidadasilva <rm86396@estudante.fieb.edu.br>
2023-04-14 10:01:37 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+        public class Main {
+            public static void main(String[] args) {
+                // laço enqunto- while
+                int contador = 0;
+                while (contador < 10) {
+                    // processos
+                    System.out.println("Contador = " + contador);
+                    contador = contador + 1; //Incremento
+                }
+                // laço faça enquanto- do while
+                int cont = 1;
+                do {
+                    System.out.println(cont);
+                    cont++; //Incremento
+                } while (cont <= 10);
+
+                // laço para faça for
+                for (int x = 0; x < 10; x++) {
+                    System.out.println(x);
+                }
+            }
+        }
+    }
+}
